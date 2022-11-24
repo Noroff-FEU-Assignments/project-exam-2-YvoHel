7772c048ee3bd5be7528ed045dacbc66f61f40b3
YvoHel <79223033+YvoHel@users.noreply.github.com>
2022-11-24 16:44:03 +0100

    add emloye

--- pages/employes-admin.js
+++ pages/employes-admin.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../components/layout/_layout";
 import MediaDropdown from "../components/about-section/_employes";
+import AddPost from "./addPost";
+import Link from "next/link";
 
 
 export default function EmployesAdmin() {
@@ -10,6 +12,7 @@ export default function EmployesAdmin() {
     <title>Admin</title>
    </Head>
    <MediaDropdown/>
+   <Link href= "/addPost"><a>Add post</a></Link>
    </Layout>
   
  );
